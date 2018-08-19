8715938169026ec6740718aa34332928e4c231c4
vagrant <vagrant@master.ansible>
2018-08-19 17:35:42 +0000

    Updated VSREmote.sh file

--- VSRemote.sh
+++ VSRemote.sh
@@ -1,5 +1,15 @@
  if [[ $# -eq 0 ]] then
  echo "Parse a file you need to open"
  fi
- ssh -i C:\\Users\\Oleksii_Pasichnyk\\.vagrant.d\\insecure_private_key -R 52698:localhost:52698 vagrant@192.168.1.2
- sudo /usr/local/bin/rmate $1
+ ssh -i $HOMEDRIVE\\$HOMEPATH\\.vagrant.d\\insecure_private_key -R 52698:localhost:52698 vagrant@192.168.1.2
+ 
+ if [[ ~/bin/rmate -e ]]; then
+ else
+ curl -Lo ~/bin/rmate https://raw.githubusercontent.com/textmate/rmate/master/bin/rmate
+ chmod a+x ~/bin/rmate
+ export PATH="$PATH:$HOME/bin"
+ 
+ fi
+ Htemp = `env |grep -oP "(?<="^HOME=").*"`
+
+ sudo ${Htemp}/bin/rmate $1
