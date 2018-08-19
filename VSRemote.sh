59ba1575ec6662e5f039632c8dcad6d20b5146b6
ImBeeMaster <36162195+ImBeeMaster@users.noreply.github.com>
2018-08-19 21:23:11 +0300

    VSRemote -- release version

--- VSRemote.sh
+++ VSRemote.sh
@@ -1,15 +1,21 @@
  if [[ $# -eq 0 ]]; then
  echo "Parse a file you need to open"
+ exit
+ else echo "Hello World"
  fi
- ssh -i $HOMEDRIVE\\$HOMEPATH\\.vagrant.d\\insecure_private_key -R 52698:localhost:52698 vagrant@192.168.1.2
+ ssh -i $HOMEDRIVE\\$HOMEPATH\\.vagrant.d\\insecure_private_key -R 52698:localhost:52698 vagrant@192.168.1.2  /bin/bash << EOSSH
  
- if [[ ~/bin/rmate -e ]]; then
- else
- curl -Lo ~/bin/rmate https://raw.githubusercontent.com/textmate/rmate/master/bin/rmate
- chmod a+x ~/bin/rmate
- export PATH="$PATH:$HOME/bin"
- 
+ if [[ -f ~/bin/rmate ]]
+  then echo "Hello World 1"
+  else echo "Hello World 2"
+  mkdir ~/bin
+  curl -Lo ~/bin/rmate https://raw.githubusercontent.com/textmate/rmate/master/bin/rmate
+  echo "Hello World 3"
+  chmod a+x ~/bin/rmate
+  echo "Hello World 4"
+  export PATH="$PATH:$HOME/bin"
  fi
- Htemp = `env |grep -oP "(?<="^HOME=").*"`
-
- sudo ${Htemp}/bin/rmate $1
+ #Htemp=$(env |grep -oP "(?<="^HOME=").*") && echo "Hello ${Htemp}"
+ #sudo ${Htemp}/bin/rmate $1
+ sudo ~/bin/rmate $1
+EOSSH
